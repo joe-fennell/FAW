317714ac137f8c777f9493d25fd7451bad5f97a4
TMCantwell <36633648+TMCantwell@users.noreply.github.com>
2019-03-15 14:01:12 +0000

    increas GPU usage fraction to 0.8

--- FAW_resnet18.py
+++ FAW_resnet18.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.7
+config.gpu_options.per_process_gpu_memory_fraction = 0.8
 set_session(tf.Session(config=config))
 import keras
 from classification_models.resnet import ResNet18
