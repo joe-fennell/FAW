efba2849ff147cccf0eae857ba147d6d033fa4b7
George <worrall.george@gmail.com>
2019-06-05 11:12:36 +0100

    fixed string join command in detector.

--- FAW_detector.py
+++ FAW_detector.py
@@ -110,5 +110,5 @@ if __name__ == "__main__":
         prediction = classifier.predict(args.path_to_img)
         print("\nFall Armyworm detected probability: " + str(prediction))
     else:
-        print("\nFAW_Detector ERROR: {} is not a valid file.".join(
+        print("\nFAW_Detector ERROR: {} is not a valid file.".format(
         args.path_to_img))
